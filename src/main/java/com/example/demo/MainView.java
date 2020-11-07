958095746f71f399625acffd69f2211840905a46
Назукин Дмитрий Евгеньевич <Nazukin.D.Ev@sberbank.ru>
2020-11-07 14:08:41 +0300

    Make example of validation loss in SuperBigDecimalField

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+
+import org.vaadin.miki.superfields.numbers.SuperBigDecimalField;
+
+import java.math.BigDecimal;
+import java.util.Locale;
 
 /**
  * The main view contains a button and a click listener.
@@ -20,25 +21,25 @@ import com.vaadin.flow.server.PWA;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-
-        // Button click listeners can be defined as lambda expressions
-        GreetService greetService = new GreetService();
-        Button button = new Button("Say hello",
-                e -> Notification.show(greetService.greet(textField.getValue())));
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
         addClassName("centered-content");
 
-        add(textField, button);
+        Binder<Model> dcdBinder = new Binder<>(Model.class);
+        SuperBigDecimalField superBigDecimalField = new SuperBigDecimalField(new Locale("RU", "ru"), Integer.MAX_VALUE);
+        dcdBinder.forField(superBigDecimalField)
+                .withValidator(it -> false, "Wrong value")
+                .bind(Model::getValue, Model::setValue);
+        add(superBigDecimalField);
+    }
+
+    public static class Model {
+        BigDecimal value;
+
+        private void setValue(final BigDecimal value) {
+            this.value = value;
+        }
+
+        private BigDecimal getValue() {
+            return value;
+        }
     }
 }
